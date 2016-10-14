989c9ebe831ad90ba53377fabfcf9ed2011eed72
Michael Spear <spear@cse.lehigh.edu>
2016-10-14 09:25:27 -0400

    Part to the project

---
+++ chimes.js
@@ -0,0 +1,9 @@
+//360000 milliseconds in a hour
+//900000 milliseconds in 15 mins.
+// create an HTTP object, for responding to requests
+// var http = require('http');
+
+// // create the objects we'll need for doing our improved file routing
+// var fs = require('fs');
+// var parse = require('url').parse;
+// var join = require('path').join;
