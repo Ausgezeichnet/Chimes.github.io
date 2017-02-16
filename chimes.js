b0a2bc50e3396cfb44b072173c4f366ebba5a844
TeddySpear <teddy@pennpals.us>
2017-02-15 20:09:21 -0500

    Some improvements, mostly variable cleanup

--- chimes.js
+++ chimes.js
@@ -1,37 +1,37 @@
 // Do all initialization inside of the document.ready
 $(document).ready(function () {
     // Take the work we did in textbox "#important" from last time and save it in there 
-    $("#important").val(localStorage.getItem("isAwesome"));
+    $("#workspace").val(localStorage.getItem("isAwesome"));
     // The click handler for the saving button
-    $('#onbutt').click(function () {
+    $('#save-btn').click(function () {
         // Tell it what to save...
-        localStorage.setItem("isAwesome", $("#important").val());
+        localStorage.setItem("isAwesome", $("#workspace").val());
         //...and print it
         console.log(localStorage.getItem("isAwesome"));
     });
     // Set the audio variable
     var audio = $("#audio")[0];
     // The click handler for the audio button
-    $('#one').click(function () {
+    $('#audio-btn').click(function () {
         // Tell it to play "audio"
         audio.play();
     });
-    $('#noid').click(function () {
-        setTimeout(robot, 3000);
+    $('#test-btn').click(function () {
+        setTimeout(testFunc, 3000);
     });
      $('#btn1').click(function () {
-        clearInterval(varrie);
+        clearInterval(timeVar);
     });
      $('#btn2').click(function () {
-        varrie = setInterval(cappycot, 1000);
+        timeVar = setInterval(timeFunc, 1000);
     });
 });
-function robot() {
+function testFunc() {
     alert('*bzzzzzzzzzzzzzz* WORKS *bzz*');
 }
-var varrie = setInterval(cappycot, 1000);
+var timeVar = setInterval(timeFunc, 1000);
 
-function cappycot() {
-    var kpq = new Date();
-    document.getElementById("sbfc").innerHTML = kpq.toLocaleTimeString();
+function timeFunc() {
+    var date = new Date();
+    document.getElementById("time-p").innerHTML = date.toLocaleTimeString();
 }
