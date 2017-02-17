31274903635c9860edbc8ba7018e1db2e6f0e050
TeddySpear <teddy@pennpals.us>
2017-02-16 21:21:10 -0500

    Some more work... still nothing much

--- chimes.js
+++ chimes.js
@@ -9,3 +9,8 @@ function timeFunc() {
 }
 // Step 1: Figure out how to add an option to a select using JQuery
 // Step 2: Use a for loop to make the start and stop selects
+$(document).ready(function () {
+    for (var i = 1; i < 13; i ++){
+        $("#starttime").append( '<option value="'+i+'">'+i+':00 AM</option>' );
+    }
+});
